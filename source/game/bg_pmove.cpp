535703d7e2096fce392ea91882cd99ffb5a935d4
ZephyrZ2712@gmail.com <ZephyrZ2712@gmail.com@5776d9f2-9662-11de-ad41-3fcdc5e0e42d>
2009-10-04 02:39:27 +0000

    Issue 11
    Increased jump velocity to see if it's more suitable

--- source/game/bg_pmove.cpp
+++ source/game/bg_pmove.cpp
@@ -2385,7 +2385,7 @@ int NumberOfWeapons(void)
 }
 
 //Commenting out to see if it helps the 'jump' issue. -Holmes
-/*int JumpHeightDeduction(void)
+int JumpHeightDeduction(void)
 {
 	int deduction=0,i;
 	if(!NumberOfWeapons())
@@ -2399,7 +2399,7 @@ int NumberOfWeapons(void)
 		deduction+=forceJumpHeight[pm->ps->fd.forcePowerLevel[FP_LEVITATION]]/10; 
 	}
 	return deduction;
-}*/
+}
 
 /*
 =============
@@ -2589,7 +2589,7 @@ static qboolean PM_CheckJump( void )
 				//check for max force jump level and cap off & cut z vel
 				if ( ( curHeight<=forceJumpHeight[0] ||//still below minimum jump height
 						(pm->ps->fd.forcePower&&pm->cmd.upmove>=10) ) &&////still have force power available and still trying to jump up 
-						curHeight < forceJumpHeight[pm->ps->fd.forcePowerLevel[FP_LEVITATION]]/*-JumpHeightDeduction()*/ &&//[Weight] -Holmes edit, commented out JumpHeightDeduction temporarily to see if it fixes the compiler error.
+						curHeight < forceJumpHeight[pm->ps->fd.forcePowerLevel[FP_LEVITATION]]-JumpHeightDeduction() &&//[Weight] -Holmes edit, commented out JumpHeightDeduction temporarily to see if it fixes the compiler error.
 					pm->ps->fd.forceJumpZStart)//still below maximum jump height
 				{//can still go up
 					if ( curHeight > forceJumpHeight[0] )
