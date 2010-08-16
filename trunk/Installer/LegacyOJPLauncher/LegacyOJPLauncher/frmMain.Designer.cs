a7a85f3b283ccad335c0738b54c1494bffb8971d
ZephyrZ2712@gmail.com <ZephyrZ2712@gmail.com@5776d9f2-9662-11de-ad41-3fcdc5e0e42d>
2010-08-16 05:34:40 +0000

    Commented out Holmes' update considering he never uploaded a fix Begun work on the updater

---
+++ trunk/Installer/LegacyOJPLauncher/LegacyOJPLauncher/frmMain.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace LegacyOJPLauncher
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNews = new System.Windows.Forms.TextBox();
+            this.grpNews = new System.Windows.Forms.GroupBox();
+            this.prgTotal = new System.Windows.Forms.ProgressBar();
+            this.prgCurrent = new System.Windows.Forms.ProgressBar();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.grpNews.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNews
+            // 
+            this.txtNews.Location = new System.Drawing.Point(7, 19);
+            this.txtNews.Multiline = true;
+            this.txtNews.Name = "txtNews";
+            this.txtNews.Size = new System.Drawing.Size(619, 178);
+            this.txtNews.TabIndex = 0;
+            // 
+            // grpNews
+            // 
+            this.grpNews.Controls.Add(this.txtNews);
+            this.grpNews.Location = new System.Drawing.Point(5, 2);
+            this.grpNews.Name = "grpNews";
+            this.grpNews.Size = new System.Drawing.Size(635, 203);
+            this.grpNews.TabIndex = 1;
+            this.grpNews.TabStop = false;
+            this.grpNews.Text = "News";
+            // 
+            // prgTotal
+            // 
+            this.prgTotal.Location = new System.Drawing.Point(12, 287);
+            this.prgTotal.Name = "prgTotal";
+            this.prgTotal.Size = new System.Drawing.Size(619, 23);
+            this.prgTotal.TabIndex = 2;
+            // 
+            // prgCurrent
+            // 
+            this.prgCurrent.Location = new System.Drawing.Point(12, 244);
+            this.prgCurrent.Name = "prgCurrent";
+            this.prgCurrent.Size = new System.Drawing.Size(619, 23);
+            this.prgCurrent.TabIndex = 3;
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Location = new System.Drawing.Point(12, 228);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(60, 13);
+            this.lblCurrent.TabIndex = 4;
+            this.lblCurrent.Text = "Current File";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(12, 271);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(75, 13);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "Total Progress";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(643, 335);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblCurrent);
+            this.Controls.Add(this.prgCurrent);
+            this.Controls.Add(this.prgTotal);
+            this.Controls.Add(this.grpNews);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmMain";
+            this.Text = "Legacy OJP Launcher";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.grpNews.ResumeLayout(false);
+            this.grpNews.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNews;
+        private System.Windows.Forms.GroupBox grpNews;
+        private System.Windows.Forms.ProgressBar prgTotal;
+        private System.Windows.Forms.ProgressBar prgCurrent;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ trunk/Installer/LegacyOJPLauncher/LegacyOJPLauncher/frmMain.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Security.Cryptography;
+
+namespace LegacyOJPLauncher
+{
+    public partial class frmMain : Form
+    {
+        //Variables
+        private XmlDocument xmlFileList;
+        string path = "http://legacyrp.com/test.xml";
+        public bool gamePathSet = false;
+        public string gamePath = "";
+
+        #region Download required variables
+
+        // The thread inside which the download happens
+        private Thread thrDownload;
+        // The stream of data retrieved from the web server
+        Stream strResponse;
+        // The stream of data that we write to the harddrive
+        Stream strLocal;
+        // The request to the web server for file information
+        HttpWebRequest webRequest;
+        // The response from the web server containing information about the file
+        HttpWebResponse webResponse;
+        // The progress of the download in percentage
+        private static int PercentProgress;
+        // The delegate which we will call from the thread to update the form
+        private delegate void UpdateProgessCallback(Int64 BytesRead, Int64 TotalBytes);
+        public string url;
+        public string filename;
+        public string folder;
+        public bool fileDownloaded = false;
+        #endregion
+
+        //Constructor if we aren't configured
+        public frmMain()
+        {
+            thrDownload = new Thread(Download);
+            InitializeComponent();
+        }
+        //Constructor if we are configured
+        public frmMain(bool gamePathSet, string gamepath)
+        {
+            InitializeComponent();
+            this.gamePath = gamepath;
+            this.gamePathSet = gamePathSet;
+        }
+        ~frmMain()
+        {
+            if (thrDownload.IsAlive)
+            {
+                thrDownload.Abort();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Download()
+        {
+            Int64 size = 0;
+            using (WebClient wcDownload = new WebClient())
+            {
+                try
+                {
+                    wcDownload.Headers.Add("User-Agent", "Mozilla/4.0+");
+                    // Create a request to the file we are downloading
+                    webRequest = (HttpWebRequest)WebRequest.Create(this.url);
+                    // Set default authentication for retrieving the file
+                    webRequest.Credentials = CredentialCache.DefaultNetworkCredentials;
+                    // Retrieve the response from the server
+                    webResponse = (HttpWebResponse)webRequest.GetResponse();
+                    // Ask the server for the file size and store it
+                    Int64 fileSize = webResponse.ContentLength;
+                    size = fileSize;
+                    // It will store the current number of bytes we retrieved from the server
+                    int bytesSize = 0;
+                    // A buffer for storing and writing the data retrieved from the server
+                    byte[] downBuffer = new byte[2048];
+
+                    strResponse = wcDownload.OpenRead(this.url);
+                    strLocal = new FileStream((this.filename + ".incomplete"), FileMode.Create, FileAccess.Write, FileShare.None);
+
+
+                    // Loop through the buffer until the buffer is empty
+                    while ((bytesSize = strResponse.Read(downBuffer, 0, downBuffer.Length)) > 0)
+                    {
+                        // Write the data from the buffer to the local hard drive
+                        strLocal.Write(downBuffer, 0, bytesSize);
+                        // Invoke the method that updates the form's label and progress bar
+                        this.Invoke(new UpdateProgessCallback(this.UpdateProgress), new object[] { strLocal.Length, fileSize });
+                    }
+                }
+                catch (WebException)
+                {
+                    MessageBox.Show("Error: Unable to access file source");
+                }
+                finally
+                {
+                    long temp = 0;
+                    try
+                    {
+                        // When the above code has ended, close the streams
+                        strResponse.Close();
+                        temp = strLocal.Length;
+                        strLocal.Close();
+                    }
+                    catch (SystemException)
+                    {
+                        //  Application.Exit();
+                    }
+                    //Rename the file to the correct name
+                    if (size.CompareTo(temp) == 0 && size != 0)
+                    {
+                        File.Move((this.filename + ".incomplete"), this.filename);
+                    }
+                    else
+                    {
+                        File.Delete(this.filename + ".incomplete");
+                    }
+                    this.fileDownloaded = true;
+                }
+            }
+        }
+        string MD5SUM(byte[] FileOrText) //Output: String<-> Input: Byte[] //
+        {
+            return BitConverter.ToString(new MD5CryptoServiceProvider().ComputeHash(FileOrText)).Replace("-", "").ToLower();
+        }
+        // The progress of the download in percentage
+        public void UpdateProgress(Int64 BytesRead, Int64 TotalBytes)
+        {
+            // Calculate the download progress in percentages
+            PercentProgress = Convert.ToInt32((BytesRead * 100) / TotalBytes);
+            // Make progress on the progress bar
+            prgCurrent.Value = PercentProgress;
+            // Display the current progress on the form
+            lblCurrent.Text = "Current File: (" + this.filename + ") - " + "Downloaded " + BytesRead + " out of " + TotalBytes + " (" + PercentProgress + "%)";
+        }
+    }
+}
